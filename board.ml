c3a23ac4e616d42ea483587ed8a85a59965de0e1
vagrant <vagrant@vm3110-2017fa>
2017-11-28 05:43:38 -0500

    Check logic done. Update board started.

--- board.ml
+++ board.ml
@@ -1,3 +1,7 @@
+(******************************************************************************)
+(***************************** TYPE DECLARATIONS ******************************)
+(******************************************************************************)
+
 type color = | White | Black
 
 type piece_rank =
@@ -17,6 +21,15 @@ type move = position * position
 type move_history = (piece * move) list
 
 type board = (position * piece) list * (position * piece) list
+
+type check = | Black_Check | White_Check | No_Check
+
+type end_game = | Checkmate | Stalemate
+
+
+(******************************************************************************)
+(*************************** BOARD INITIALIZATION *****************************)
+(******************************************************************************)
 
 let rec setup_board color =
   match color with
@@ -49,78 +62,24 @@ and setup_back color rank =
 
 let init_board = ((setup_board Black), (setup_board White))
 
-type check = | Black_Check | White_Check | No_Check
 
-type end_game = | Checkmate | Stalemate
+(*************************************************************)
+(************************ GAME LOGIC *************************)
+(*************************************************************)
 
-let is_check board color move =
-(*
-  if color = Black
-  then
-    let pieces = fst board in
-    failwith "unimplemented"
-  else *)
-    failwith "unimplemented"
 
-let attack_array =
-   [0;0;0;0;0;0;0;0;0;5;0;0;0;0;0;0;2;0;0;0;     (* 0-19 *)
-    0;0;0;5;0;0;5;0;0;0;0;0;2;0;0;0;0;0;5;0;     (* 20-39 *)
-    0;0;0;5;0;0;0;0;2;0;0;0;0;5;0;0;0;0;0;0;     (* 40-59 *)
-    5;0;0;0;2;0;0;0;5;0;0;0;0;0;0;0;0;5;0;0;     (* 60-79 *)
-    2;0;0;5;0;0;0;0;0;0;0;0;0;0;5;6;2;6;5;0;     (* 80-99 *)
-    0;0;0;0;0;0;0;0;0;0;6;4;1;4;6;0;0;0;0;0;     (* 100-119 *)
-    0;2;2;2;2;2;2;1;0;1;2;2;2;2;2;2;0;0;0;0;     (* 120-139 *)
-    0;0;6;3;1;3;6;0;0;0;0;0;0;0;0;0;0;0;5;6;     (* 140-159 *)
-    2;6;5;0;0;0;0;0;0;0;0;0;0;5;0;0;2;0;0;5;     (* 160-179 *)
-    0;0;0;0;0;0;0;0;5;0;0;0;2;0;0;0;5;0;0;0;     (* 180-199 *)
-    0;0;0;5;0;0;0;0;2;0;0;0;0;5;0;0;0;0;5;0;     (* 200-219 *)
-    0;0;0;0;2;0;0;0;0;0;5;0;0;5;0;0;0;0;0;0;     (* 220-239 *)
-    2;0;0;0;0;0;0;5;0;0;0;0;0;0;0;0;0         ]  (* 240-256 *)
-
-let fr2sq (f,r) = 16 * (r-1) + (f-1)
-
-let att_constants = [
-  ("k" , [1;3;4]);
-  ("q" , [2;3;4;5]);
-  ("r" , [1;2]);
-  ("bw", [3;5]);
-  ("bb", [4;5]);
-  ("n" , [6]);
-  ("p" , [3;4]);
-]
-
-let is_attacked p (fa,ra) (fd,rd) =
-  let parray =
-    match snd p with
-    | King   -> List.assoc "k" att_constants
-    | Queen  -> List.assoc "q" att_constants
-    | Rook   -> List.assoc "r" att_constants
-    | Bishop ->
-      if fst p = Black
-      then List.assoc "bb" att_constants
-      else List.assoc "bw" att_constants
-    | Knight -> List.assoc "n" att_constants
-    | Pawn _ -> List.assoc "p" att_constants in
-  let att_sq = fr2sq (fa,ra) in
-  let def_sq = fr2sq (fd,rd) in
-  let formula = def_sq - att_sq + 128 in
-  List.mem (List.nth attack_array formula) parray
-
+(********************* PSUEDO-MOVE LOGIC *********************)
 
 (* [moves p pos] returns a list positions that piece [p] can move to, given that
  * it is in position [pos] on board [b]. *)
 let rec moves b mh p (f,r) =
   match (snd p) with
-  | King -> moves_k b (fst p) (f,r)
+  | King -> moves_k b mh (fst p) (f,r)
   | Queen -> moves_q b (fst p) (f,r)
   | Rook -> moves_r b (fst p) (f,r)
   | Knight -> moves_n b (fst p) (f,r)
   | Bishop -> moves_b b (fst p) (f,r)
   | Pawn moved -> moves_p b mh (fst p) moved (f,r)
-
-(* and in_bounds (f,r) =
-  if 1<=f && f<=8 && 1<=r && r<=8
-  then [(f,r)] else [] *)
 
 and in_bounds (f,r) = 1<=f && f<=8 && 1<=r && r<=8
 
@@ -147,11 +106,16 @@ and check_dir b c (f,r) (dx,dy) lst =
       else new_space::lst
     | None -> check_dir b c new_space (dx,dy) (new_space::lst)
 
-and moves_k b c (f,r) =
-  moveable_space b c (f+1,r) @
-  moveable_space b c (f-1,r) @
-  moveable_space b c (f,r+1) @
-  moveable_space b c (f,r-1)
+and moves_k b mh c (f,r) =
+  let e = moveable_space b c (f+1,r) in
+  let w = moveable_space b c (f-1,r) in
+  let n = moveable_space b c (f,r+1) in
+  let s = moveable_space b c (f,r-1) in
+
+  let rank = match c with | Black -> 8 | White -> 1 in
+  let castle = [] in (* TODO *)
+  e @ w @ n @ s @ castle
+
 
 and moves_r b c (f,r) =
   let n  = check_dir b c (f,r) (1 , 0) [] in
@@ -203,8 +167,112 @@ and moves_p b mh c moved (f,r) =
       else [] in
   forward @ forward_left @ forward_right @ two_sq @ en_pass
 
-let legal_moves b c = failwith "unimplemented"
+
+(************************ CHECK LOGIC ************************)
+
+let rec is_check b c =
+  let opp_pieces = match c with | Black -> snd b | White -> fst b in
+  let k_pos =
+    match c with
+    | Black -> find_king (fst b)
+    | White -> find_king (snd b) in
+  if is_attacked b opp_pieces k_pos
+  then
+    match c with
+    | Black -> Black_Check
+    | White -> White_Check
+  else No_Check
+
+
+and find_king ps =
+  try
+    fst (List.find
+      (fun (c,(pos,piece)) -> match piece with
+       | King -> true
+       | _ -> false)
+      ps)
+  with Not_found -> failwith "Somehow captured a king."
+
+(* Attack array and associated logic from Jonatan Pettersson at
+ * http://mediocrechess.blogspot.com/2006/12/guide-attacked-squares.html *)
+and attack_array =
+   [0;0;0;0;0;0;0;0;0;5;0;0;0;0;0;0;2;0;0;0;     (* 0-19 *)
+    0;0;0;5;0;0;5;0;0;0;0;0;2;0;0;0;0;0;5;0;     (* 20-39 *)
+    0;0;0;5;0;0;0;0;2;0;0;0;0;5;0;0;0;0;0;0;     (* 40-59 *)
+    5;0;0;0;2;0;0;0;5;0;0;0;0;0;0;0;0;5;0;0;     (* 60-79 *)
+    2;0;0;5;0;0;0;0;0;0;0;0;0;0;5;6;2;6;5;0;     (* 80-99 *)
+    0;0;0;0;0;0;0;0;0;0;6;4;1;4;6;0;0;0;0;0;     (* 100-119 *)
+    0;2;2;2;2;2;2;1;0;1;2;2;2;2;2;2;0;0;0;0;     (* 120-139 *)
+    0;0;6;3;1;3;6;0;0;0;0;0;0;0;0;0;0;0;5;6;     (* 140-159 *)
+    2;6;5;0;0;0;0;0;0;0;0;0;0;5;0;0;2;0;0;5;     (* 160-179 *)
+    0;0;0;0;0;0;0;0;5;0;0;0;2;0;0;0;5;0;0;0;     (* 180-199 *)
+    0;0;0;5;0;0;0;0;2;0;0;0;0;5;0;0;0;0;5;0;     (* 200-219 *)
+    0;0;0;0;2;0;0;0;0;0;5;0;0;5;0;0;0;0;0;0;     (* 220-239 *)
+    2;0;0;0;0;0;0;5;0;0;0;0;0;0;0;0;0         ]  (* 240-256 *)
+
+and fr2sq (f,r) = 16 * (r-1) + (f-1)
+
+and att_constants = [
+  ("k" , [1;3;4]);
+  ("q" , [2;3;4;5]);
+  ("r" , [1;2]);
+  ("bw", [3;5]);
+  ("bb", [4;5]);
+  ("n" , [6]);
+  ("p" , [3;4]);
+]
+
+and can_attack p (fa,ra) (fd,rd) =
+  let parray =
+    match snd p with
+    | King   -> List.assoc "k" att_constants
+    | Queen  -> List.assoc "q" att_constants
+    | Rook   -> List.assoc "r" att_constants
+    | Bishop ->
+      if fst p = Black
+      then List.assoc "bb" att_constants
+      else List.assoc "bw" att_constants
+    | Knight -> List.assoc "n" att_constants
+    | Pawn _ -> List.assoc "p" att_constants in
+  let att_sq = fr2sq (fa,ra) in
+  let def_sq = fr2sq (fd,rd) in
+  let formula = def_sq - att_sq + 128 in
+  List.mem (List.nth attack_array formula) parray
+
+and is_attacked b opp_ps d_pos =
+  match opp_ps with
+  | [] -> false
+  | ((f,r), piece)::t ->
+    if (snd piece) != King && can_attack piece (f,r) d_pos
+    then
+      if List.mem d_pos (moves b [] piece (f,r))
+      then true (*  match c with | Black -> Black_Check | White -> White_Check *)
+      else is_attacked b t d_pos
+    else is_attacked b t d_pos
+
+
+(********************* BOARD UPDATE LOGIC **********************)
+
+
+let update_board b mh c m =
+  let i_pos = fst m in
+  let f_pos = snd m in
+  let bl_ps = fst b in
+  let wh_ps = snd b in
+  let piece =
+    match c with
+    | Black -> List.assoc i_pos bl_ps
+    | White -> List.assoc i_pos wh_ps in
+
+  if List.mem f_pos (moves b mh snd piece i_pos)
+  then
+    let bl_ps' = (List.remove_assoc i_pos bl_ps) in
+    let bl_ps'' = (f_pos,piece)::bl_ps' in
+    let b' = (bl_ps'',wh_ps) in
+    failwith "unimplemented"
+
+  else b
+
+let promote b (f,r) = failwith "unimplemented"
 
 let end_type b = failwith "unimplemented"
-
-let update_board b m = failwith "unimplemented"
