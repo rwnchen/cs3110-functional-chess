99684c7e3d03bc9b5ed62004a8d58eb8f1b7777e
Clownpiece <Clownpiece@Hello>
2017-11-30 16:42:53 -0500

    Opening parsing wip

--- opener.ml
+++ opener.ml
@@ -1,7 +1,7 @@
 open Str
-open Trie
 open Board
 open Yojson.Basic.Util
+open String
 (* ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *)
 (* Representation types *)
 
@@ -14,8 +14,6 @@ type opmetadata =
     category : string; (* ECO category *)
   }
 
-type opmoves = opmetadata * move
-
 type openings = ()
 
 (* ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *)
@@ -23,8 +21,8 @@ type openings = ()
 
 (* Trie database *)
 module StrMap = Map.Make(String)
-module StrTrie = Make(StrMap)
-let init_database = StrTrie.empty (* Empty database *)
+module StrTrie = Trie.Make(StrMap)
+let init_openings = StrTrie.empty (* Empty openings database *)
 
 (* Regex used to parse string lines containing the game moves. *)
 let moves_regex = regexp "."
@@ -34,11 +32,42 @@ let dir = "openings/"
 let eco_opening_json = dir ^ "eco_openings.json"
 let ficsgames_1 = dir ^ "ficsgames_1.pgn"
 
+(* Ported from a2 - helper function to parse all elements
+ * in a Yojson String list to an actual string list *)
+let rec json_to_string_list = function
+  | [] -> []
+  | h::t ->
+    (h |> to_string)::(json_to_string_list t)
+
 let load_png f = None
 
-let init_eco_names f =
+(* [parse_eco f]
+ * string -> string list list
+ * Given the path of a json file [f], parses [f] into a list of ECO chess
+ * openings. Each opening in the list is a string list, with at least 2
+ * elements. The first (head) element is the name of the opening, and the
+ * following elements are the moves associated with that opening. *)
+let parse_eco f =
   let js = Yojson.Basic.from_channel (open_in f) in
-  None
+  let eco_list_js = js |> member "openings" |> to_list in
+  let rec to_eco_list l r =
+    match l with
+    | [] -> r
+    | h::t -> to_eco_list t ((h |> to_list |> json_to_string_list)::r)
+  in
+  to_eco_list eco_list_js []
+  (*List.iter (fun l -> List.iter (fun x -> Printf.printf "%s\t" x) l) eco*)
+
+(* [init_eco f]
+ * string -> openings
+ * Constructs the openings trie given a json file [f] with all openings and
+ * their associated names. The metadata stored in the trie only contains the
+ * opening name associated with its key; other statistics are left to default
+ * values.
+ * This is the 'initial' trie. *)
+let init_eco f =
+  let eco_list = parse_eco f in
+  ()
 
 (* ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *)
 (* Exposed functions *)
