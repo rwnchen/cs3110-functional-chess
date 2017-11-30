83eecd7ec55331fdc37b0bf6d2635a91921501fd
Clownpiece <Clownpiece@Hello>
2017-11-30 15:41:08 -0500

    Unk

--- opener.ml
+++ opener.ml
@@ -20,13 +20,22 @@ type openings = ()
 (* ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *)
 (* Helper functions/states *)
 
+(* Trie database *)
+module StrMap = Map.Make(String)
+module StrTrie = Make(StrMap)
+let init_database = StrTrie.empty (* Empty database *)
+
 (* Regex used to parse string lines containing the game moves. *)
 let moves_regex = regexp "."
+
+
 
 (* ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *)
 (* Exposed functions *)
 
-let init_openings f = failwith "init_openings: Unimplemented"
+let init_openings f =
+  let replays = load_png f in
+  let 
 
 let opening_name o = failwith "opening_name: Unimplemented"
 
