c65da77e6e20dd7d307c4085a61cb53d81ac847c
rgw66 <rgw66@cornell.edu>
2017-12-03 19:37:25 -0500

    gui sort of working with the board

--- gui.py
+++ gui.py
@@ -176,8 +176,8 @@ class GameBoard(tk.Frame):
         piece = self.getpieceatpos(pos1)
         if piece is not None:
             origin = self.pieces[piece]
-            dx = pos2[0] - pos1[0]
-            dy = pos2[1] - pos1[1]
+            dy = pos1[0] - pos2[0]
+            dx = pos1[1] - pos2[1]
             self.canvas.move(piece, dy*self.size, dx*self.size)
             self.pieces[piece] = (pos2[0],pos2[1])
             return piece
@@ -185,29 +185,42 @@ class GameBoard(tk.Frame):
             return "none"
 
     def callback(self, event):
-        #clear highlighted squars after every click
+        #clear highlighted squares after every click
         item = event.widget.find_withtag("current")
         tags = self.canvas.itemcget(item, "tags").split(" ")
-        name = tags[0]
-        if ("square" in tags or "highlighted" in tags):
-            name = tags[1]
-        elif (name in self.pieces):
-            pos = self.pieces[name]
-            name = str(pos[0]) + str(pos[1])
+        name = []
+        if ("square" in tags):
+            name = [u"empty",tags[1]]
+        elif ("highlighted" in tags):
+            name = [u"highlight",tags[1]]
+        elif (tags[0] in self.pieces):
+            pos = self.pieces[tags[0]]
+            name = [u"piece",str(pos[0]) + str(pos[1])]
+        else:
+            name = [u"none","99"]
         self.canvas.delete("highlighted")
         self.highlighted = []
         self.was_click = True
         self.clicked_space = name
 
-def get_piece(board):
+def get_click(board):
     clicked_space = list(board.clicked_space)
-    return [int(clicked_space[0]),int(clicked_space[1])]
+    click_type = clicked_space[0]
+    if click_type != u"none":
+        click_pos = [int(clicked_space[1][0])+1,int(clicked_space[1][1])+1]
+    else:
+        click_pos = [-1,-1]
+    return [unicode(click_type),click_pos]
 
 def move(board, pos1, pos2):
+    pos1 = (pos1[0]-1,pos1[1]-1)
+    pos2 = (pos2[0]-1,pos2[1]-1)
     board.move(pos1,pos2)
     return board
 
 def highlight(board, x, y):
+    x -= 1
+    y -= 1
     if ((x,y) not in board.highlighted):
         board.highlight_rect(x,(y-7)*-1)
     return board
@@ -226,7 +239,7 @@ def update_game(board, update):
     board.root.update()
     click = board.was_click
     board.was_click = False
-    return [board, click]
+    return click
 
 
 if __name__ == "__main__":
